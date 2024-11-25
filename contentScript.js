24a1328ff426b00b5f19c5b7c249cd9070fe769f
Jas Dhaul <jasdhaul0@gmail.com>
2024-11-25 14:58:50 -0800

    Simplified code.

--- contentScript.js
+++ contentScript.js
@@ -1,16 +1,12 @@
-// Monitor for job listing page
 const LINKEDIN_JOBS_URL_PATTERN = /linkedin\.com\/jobs\//;
 
 function isJobListingPage() {
     return LINKEDIN_JOBS_URL_PATTERN.test(window.location.href);
 }
 
+console.log("LinkedIn Jobs Blocker: Content script loaded");
 
 async function init() {
-    try {
-        if (!isJobListingPage()) return;
-
-        console.log("LinkedIn Jobs page detected");
-    }
+    if (!isJobListingPage()) return;
+    console.log("LinkedIn Jobs page detected");
 }
-
