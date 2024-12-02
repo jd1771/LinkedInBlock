62c8df4bc0d765a62873948b0a35dff4305385b7
Jas Dhaul <jasdhaul0@gmail.com>
2024-12-02 10:59:56 -0800

    Update restore svg code.

--- viewScript.js
+++ viewScript.js
@@ -29,10 +29,10 @@ function populateBlockedListings() {
                             ${companyName}
                         </a>
                         
-                        <button class="unblock-btn" aria-label="Unblock ${companyName}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="x-icon">
-                                <line x1="18" y1="6" x2="6" y2="18"></line>
-                                <line x1="6" y1="6" x2="18" y2="18"></line>
+                        <button class="unblock-btn" aria-label="Restore ${companyName}">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="restore-icon">
+                                <polyline points="9 1 4 6 9 11"></polyline>
+                                <path d="M20 17.58A9 9 0 0 0 6.36 6.36L4 8"></path>
                             </svg>
                         </button>
                     `;
